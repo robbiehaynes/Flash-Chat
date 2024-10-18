b90af454b69497e053f7073f17898d53dba4c235
Robert Haynes <robhaynes0420@gmail.com>
2024-10-18 19:35:37 +0100

    MessageCell TableViewCells
    Custom MessageCell.xib files for the design of the chat bubbles.

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Robert Haynes on 2024/10/18.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
